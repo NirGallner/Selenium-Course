fdacfdb472749f99490a6939b574e11cbca15773
nir@verisoft.co <nir@verisoft.co>
2018-01-27 00:24:56 +0200

    message

---
+++ com.nir.selenium/src/main/java/com/nir/selenium/TestNgPlayground.java
@@ -0,0 +1,12 @@
+package com.nir.selenium;
+
+import org.testng.annotations.Test;
+
+public class TestNgPlayground {
+	
+	@Test
+	public void test1() {
+		System.out.println("Hello, TestNG");
+	}
+
+}
